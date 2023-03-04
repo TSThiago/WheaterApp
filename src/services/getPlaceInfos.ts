12ce54f809c724101fb806e253a14cfbdb1e7d4f
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2023-03-03 23:39:22 -0300

    debugando o código

--- src/services/getPlaceInfos.ts
+++ src/services/getPlaceInfos.ts
@@ -2,7 +2,7 @@ import adaptGetPlaceInfos from "../shared/adapters/adaptGetPlaceInfos"
 
 const getPlaceInfos = async (place: string) => {
     place.split('').join('_')
-    const response = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + place + '&appid=febfe56ed42722d4c8aed44ff8d064a3&lang=pt')
+    const response = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + place + '&appid=febfe56ed42722d4c8aed44ff8d064a3')
         .then(res => res.json())
     const adaptedResponse = adaptGetPlaceInfos(response[0])
     return adaptedResponse;
